174e0ab5d23e77ac546961b85046f23140dbdecf
Brock Wilcox <awwaiid@thelackthereof.org>
2026-07-24 15:02:33 -0400

    sketch python sketchy thing

---
+++ drawstuff.py
@@ -0,0 +1,22 @@
+
+from textwrap import dedent
+
+def d(str):
+    return dedent(str)[1:-1]
+
+memory_cell = d("""
+    +-------+
+    | V   W<|
+    |@>RXWS^|
+    |   R   |
+    |   W   |
+    | ^ <   |
+    +-------+
+""")
+
+# So then... imagine ....
+# proggie = Proggie()
+# proggie.draw_at(x, y, memory_cell)
+#
+# proggie.print()
+
